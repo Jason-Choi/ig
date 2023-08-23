d4f6dc83c465a8f828231776b88aeb0572187e96
jason-choi-skku <jasonchoi3@g.skku.edu>
2023-08-24 02:01:52 +0900

    fix

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const setNameandSoosickfromDB = (name) => {
   const nameHTML = document.getElementById("name")
   const soosickHTML = document.getElementById("soosick")
   const user = db.find((user) => user.name === name)
-  nameHTML.innerHTML = user ? "호시노 " + name.slice(1, name.length) + "님" : "당신"
+  nameHTML.innerHTML = name ? "호시노 " + name.slice(1, name.length) + "님" : "당신"
   soosickHTML.innerHTML = user ? user.soosick : "존예존잘프리티핸섬섹시큐티"
 }
 
