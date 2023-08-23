4b8d344c89526be5ff0bf44a4f51f70d12c87b4d
jason-choi-skku <jasonchoi3@g.skku.edu>
2023-08-24 02:20:38 +0900

    custom soosick

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const db = [{ name: "정일묵", soosick: "타이 프린스" }]
 
-const setNameandSoosickfromDB = (name) => {
+const setNameandSoosickfromDB = (params) => {
   const nameHTML = document.getElementById("name")
   const soosickHTML = document.getElementById("soosick")
-  const user = db.find((user) => user.name === name)
-  nameHTML.innerHTML = name ? "호시노 " + name.slice(1, name.length) + "님" : "당신"
-  soosickHTML.innerHTML = user ? user.soosick : "존예존잘프리티핸섬섹시큐티"
+  const user = db.find((user) => user.name === params.get("name"))
+  nameHTML.innerHTML = params.get("name") ? "호시노 " + params.get("name").slice(1, params.get("name").length) + "님" : "당신"
+  soosickHTML.innerHTML = params.get("soosick") ? params.get("soosick") : user ? user.soosick : "존예존잘프리티핸섬섹시큐티"
 }
 
 const params = new URLSearchParams(window.location.search)
-setNameandSoosickfromDB(params.get("name"))
+setNameandSoosickfromDB(params)
