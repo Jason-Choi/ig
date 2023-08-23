d48ca8ae49406f1526d552c4e6710f0f3d7abc4f
jason-choi-skku <jasonchoi3@g.skku.edu>
2023-08-24 01:48:10 +0900

    no fn

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const db = [{ name: "정일묵", soosick: "타이 프린스" }]
 const setNameandSoosickfromDB = (name) => {
   const nameHTML = document.getElementById("name")
   const soosickHTML = document.getElementById("soosick")
-  nameHTML.innerHTML = name
+  nameHTML.innerHTML = name.slice(1, name.length - 1)
   const user = db.find((user) => user.name === name)
   soosickHTML.innerHTML = user ? user.soosick : "존예존잘프리티핸섬섹시큐티"
 }
